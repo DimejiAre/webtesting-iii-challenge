02c1c32548d09c6a092856b63d723b541b3e20ca
DimejiAre <olardi2@gmail.com>
2019-10-14 13:36:50 +0100

    create Gate and Dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,34 @@
-// Test away
+import React from 'react';
+import Dashboard from './Dashboard';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+afterEach(rtl.cleanup)
+
+let wrapper, closeButton, lockButton;
+
+beforeEach(()=>{
+    wrapper = rtl.render(<Dashboard />);
+    closeButton = wrapper.queryByText(/close gate/i);
+    lockButton = wrapper.queryByText(/lock gate/i);
+})
+
+describe('Dashboard Component', ()=>{
+    it('Shows controls and Display', ()=>{
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    })
+
+    it('Defaults to Unlocked and Open', ()=>{
+        expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+        expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+    })
+
+    it('Cannot be closed or opened if locked', ()=>{
+        rtl.fireEvent.click(closeButton)
+        rtl.fireEvent.click(lockButton)
+        expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+    })
+})
